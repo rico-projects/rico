527ba83c3350c678b83f606d3f3d0eb2eb0c7d4e
Hendrik Ebbers <hendrik.ebbers@web.de>
2018-12-23 13:45:02 +0100

    Interceptor for JavaEE done

---
+++ base/rico-server-javaee/src/main/java/dev/rico/internal/server/timing/TimingInterceptor.java
@@ -0,0 +1,43 @@
+package dev.rico.internal.server.timing;
+
+import dev.rico.internal.core.Assert;
+import dev.rico.server.timing.Metric;
+import dev.rico.server.timing.ServerTiming;
+import dev.rico.server.timing.Timing;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+@Timing
+@Interceptor
+public class TimingInterceptor {
+
+    private final ServerTiming serverTiming;
+
+    @Inject
+    public TimingInterceptor(final ServerTiming serverTiming) {
+        this.serverTiming = Assert.requireNonNull(serverTiming, "serverTiming");
+    }
+
+    @AroundInvoke
+    public Object handleFoo(InvocationContext joinPoint) throws Exception {
+        final Method m = joinPoint.getMethod();
+        final Timing timingAnnotation = m.getAnnotation(Timing.class);
+        if(timingAnnotation != null) {
+            final String name = Optional.ofNullable(timingAnnotation.value()).orElse("UNKNOWN");
+            final String description = timingAnnotation.description();
+            final Metric metric = serverTiming.start(name, description);
+            try {
+                return joinPoint.proceed();
+            } finally {
+                metric.stop();
+            }
+        } else {
+            return joinPoint.proceed();
+        }
+    }
+}
