21a83f02da1d577ca33eee60ed606b31d4b21c9c
Hendrik Ebbers <hendrik.ebbers@me.com>
2019-01-29 15:43:16 +0100

    Update integration-tests/integration-tests/src/test/java/dev/rico/integrationtests/ModelHierarchyControllerTest.java

--- integration-tests/integration-tests/src/test/java/dev/rico/integrationtests/ModelHierarchyControllerTest.java
+++ integration-tests/integration-tests/src/test/java/dev/rico/integrationtests/ModelHierarchyControllerTest.java
@@ -19,7 +19,7 @@ public class ModelHierarchyControllerTest extends AbstractIntegrationTest {
     @Test(dataProvider = ENDPOINTS_DATAPROVIDER)
     public void testSetChildModels(final String containerType, final String endpoint) {
         try {
-            ClientContext context = connect(endpoint);
+            final ClientContext context = connect(endpoint);
             ControllerProxy<RootModel> controller = createController(context, CONTROLLER_NAME);
             controller.invoke(A_TO_RANDOM_ACTION).get();
             controller.invoke(B_TO_RANDOM_ACTION).get();
