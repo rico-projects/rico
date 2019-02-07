0dfefb5b2daa83465b915228c17b4a533ebc8506
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-02-07 09:54:20 +0100

    Model

--- incubation/rico-projector-common/src/main/java/dev/rico/internal/projection/routing/Route.java
+++ incubation/rico-projector-common/src/main/java/dev/rico/internal/projection/routing/Route.java
@@ -1,6 +1,8 @@
 package dev.rico.internal.projection.routing;
 
 import dev.rico.internal.projection.base.Projectable;
+import dev.rico.internal.projection.metadata.KeyValue;
+import dev.rico.remoting.ObservableList;
 import dev.rico.remoting.Property;
 import dev.rico.remoting.RemotingBean;
 
@@ -10,6 +12,8 @@ public interface Route extends Projectable {
     Property<String> anchorProperty();
 
     Property<String> locationProperty();
+
+    ObservableList<KeyValue> getParameters();
 
     default String getAnchor() {
         return anchorProperty().get();
