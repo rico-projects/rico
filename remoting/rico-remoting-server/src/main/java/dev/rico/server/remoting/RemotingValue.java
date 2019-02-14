b52764371ce7335bd23048c7d73aa487564b8cd5
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-02-14 08:14:20 +0100

    JavaDoc for public API

--- remoting/rico-remoting-server/src/main/java/dev/rico/server/remoting/RemotingValue.java
+++ remoting/rico-remoting-server/src/main/java/dev/rico/server/remoting/RemotingValue.java
@@ -10,13 +10,29 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 
+/**
+ * Annotation to define inital parameters of a {@link RemotingController}. Such parameters
+ * can be defined on the client side when requesting a controller(proxy). By doing so such
+ * parameters will automatically be injected in the new controller instance before any
+ * {@link javax.annotation.PostConstruct} method is called.
+ */
 @Documented
 @Retention(RUNTIME)
 @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
 @API(since = "0.x", status = EXPERIMENTAL)
 public @interface RemotingValue {
 
+    /**
+     * Defines the name of the value. If the default ("" - empty string) is used the name of the
+     * annotated field will be used instead.
+     * @return the name of the value
+     */
     String value() default "";
 
+    /**
+     * Defines if the value is an optional value. If a value is not optional and no parameter for
+     * the value is defined, the controller can not be created and an exception will be thrown.
+     * @return true if the value is optional otherwise false
+     */
     boolean optional() default true;
 }
