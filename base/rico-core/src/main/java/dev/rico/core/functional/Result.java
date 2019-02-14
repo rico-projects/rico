8ba28a6bd60e4caccca047595189d7c993c78e57
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-02-14 09:12:41 +0100

    Javadoc

--- base/rico-core/src/main/java/dev/rico/core/functional/Result.java
+++ base/rico-core/src/main/java/dev/rico/core/functional/Result.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018-2019 Karakun AG.
+ * Copyright 2015-2018 Canoo Engineering AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.rico.core.functional;
 
 import dev.rico.internal.core.functional.Fail;
@@ -5,14 +21,42 @@ import dev.rico.internal.core.functional.Sucess;
 
 import java.util.function.Function;
 
+/**
+ * Wrapper for a result of a functional call (like {@link CheckedFunction}). The result can hold the outcome of a function
+ * or an excepton if the function was aborted exeptionally.
+ * @param <R> type of the outcome
+ */
 public interface Result<R> {
 
-    R getResult();
+    /**
+     * Returns the outcome /result of the based functional call or throws an {@link IllegalStateException} if the
+     * function was aborded by an exception. Such behavior can easily be checked by calling {@link Result#iSuccessful()}
+     * @return the outcome
+     * @throws IllegalStateException the exception if the based function was not executed sucessfully
+     */
+    R getResult() throws IllegalStateException;
 
+    /**
+     * Returns true if the based function was executed sucessfully, otherwise false.
+     * @return true if the based function was executed sucessfully, otherwise false.
+     */
     boolean iSuccessful();
 
-    Exception getException();
+    /**
+     * Returns the exception of the based functional call or throws an {@link IllegalStateException} if the
+     * function was executed sucessfully. Such behavior can easily be checked by calling {@link Result#iSuccessful()}
+     * @return the exception
+     * @throws IllegalStateException the exception if the based function was executed sucessfully
+     */
+    Exception getException() throws IllegalStateException;
 
+    /**
+     * Wraps a given {@link CheckedFunction} in a {@link Function} that returns the {@link Result} of the given {@link CheckedFunction}
+     * @param function the function
+     * @param <A> type of the input parameter
+     * @param <B> type of the result
+     * @return a {@link Function} that returns the {@link Result} of the given {@link CheckedFunction}
+     */
     static <A, B> Function<A, Result<B>> of(final CheckedFunction<A, B> function) {
         return (a) -> {
             try {
@@ -24,6 +68,13 @@ public interface Result<R> {
         };
     }
 
+    /**
+     * Wraps a given {@link CheckedFunction} in a {@link Function} that returns the {@link Result} of the given {@link CheckedFunction}
+     * @param function the function
+     * @param <A> type of the input parameter
+     * @param <B> type of the result
+     * @return a {@link Function} that returns the {@link Result} of the given {@link CheckedFunction}
+     */
     static <A, B> Function<A, ResultWithInput<A, B>> withInput(final CheckedFunction<A, B> function) {
         return (a) -> {
             try {
--- base/rico-core/src/main/java/dev/rico/internal/core/lang/DefaultTuple.java
+++ base/rico-core/src/main/java/dev/rico/internal/core/lang/DefaultTuple.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright 2018-2019 Karakun AG.
+ * Copyright 2015-2018 Canoo Engineering AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.rico.internal.core.lang;
 
 import dev.rico.core.lang.Tuple;
 
 import java.util.Objects;
 
+/**
+ * Default implementation of {@link Tuple}
+ * @param <K> type of the key
+ * @param <V> type of the value
+ */
 public class DefaultTuple<K, V> implements Tuple<K, V> {
 
     private final K key;
