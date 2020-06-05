58cbeaa8c5c7002ddb99ffb4a9dc6d1618a3241c
Stephan Classen <st.classen@gmx.ch>
2020-06-05 15:11:47 +0200

    remove mention of canoo in new file

--- base/rico-core/src/main/java/dev/rico/internal/core/context/ContextManagerImpl.java
+++ base/rico-core/src/main/java/dev/rico/internal/core/context/ContextManagerImpl.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2018-2019 Karakun AG.
- * Copyright 2015-2018 Canoo Engineering AG.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
