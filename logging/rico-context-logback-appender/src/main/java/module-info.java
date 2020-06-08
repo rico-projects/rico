6762872afca4483f7141c577d6b1ea6bffac538c
Stephan Classen <st.classen@gmx.ch>
2020-06-08 12:45:39 +0200

    add logback appender

---
+++ logging/rico-context-logback-appender/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module dev.rico.logback.appender {
+
+    exports dev.rico.logback.appender;
+
+    requires transitive dev.rico.core;
+    requires logback.classic;
+    requires logback.core;
+    requires org.slf4j;
+}
