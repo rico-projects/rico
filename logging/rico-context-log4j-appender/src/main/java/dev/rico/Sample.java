83f875ddf9a36fcd2e7d1cc2ab534fa4b4610bc5
Hendrik Ebbers <hendrik.ebbers@web.de>
2020-06-08 14:37:20 +0200

    Sample started

--- logging/rico-context-log4j-appender/src/main/java/dev/rico/Sample.java
+++ logging/rico-context-log4j-appender/src/main/java/dev/rico/Sample.java
@@ -1,6 +1,16 @@
 package dev.rico;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Sample {
 
+    private static Logger LOG = LogManager.getLogger(Sample.class);
+
+    public static void main(String[] args) {
+        LOG.error("Hi Error");
+        LOG.info("Hi Info");
+        LOG.trace("Hi Trace");
+    }
 
 }
--- logging/rico-context-log4j-appender/src/main/java/module-info.java
+++ logging/rico-context-log4j-appender/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module dev.context.log4j.appender {
 
-    requires transitive dev.rico.core;
     requires static org.apiguardian.api;
     requires org.apache.logging.log4j;
 }
